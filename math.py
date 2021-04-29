7f9a6d8258ac1994d02bbf2a19201a0f49495c86
subhadeep-deyashi-2 <subhadeep_deyashi@persistent.com>
2021-04-29 09:24:37 +0530

    added fn headers

--- math.py
+++ math.py
@@ -5,13 +5,18 @@ def add(x,y):
 # subtract implementation
 def subtract(x,y):
     return x-y #in master
+    
+# multiplication implementation
 def multiply(x,y):
     return x*y #in bug4 branch
+    
+# division implementation
 def divide(x,y):
     if y!=0:
         return x/y
     else:
         return DIVIDE_BY_0_ERROR #rebase conflict resolved
 
+# square implementation
 def square(x):
     return x*x
