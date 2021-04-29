3eafad35772feea7234337ae9ccf3c8ea5dbb732
Subhadeep Deyashi <61182822+subhadeep-deyashi-2@users.noreply.github.com>
2021-04-29 09:18:42 +0530

    math.py added

---
+++ math.py
@@ -0,0 +1,17 @@
+# Add implementation
+def add(x,y):
+    return x+y
+
+# subtract implementation
+def subtract(x,y):
+    return x-y #in master
+def multiply(x,y):
+    return x*y #in bug4 branch
+def divide(x,y):
+    if y!=0:
+        return x/y
+    else:
+        return DIVIDE_BY_0_ERROR #rebase conflict resolved
+
+def square(x):
+    return x*y
